6c20a26a85a56ba24533f6e8389e640eda976ebc
ruina.me <umewethey@gmail.com>
2024-02-09 15:45:10 +0400

    main

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 import cv2
 
-# Read the video
-cap = cv2.VideoCapture(0)
 
-# Read the first frameй
-ret, frame = cap.read()
-def take_frame():
-
-
-# Initialize the tracker
-tracker = cv2.legacy.TrackerMedianFlow_create()
-tracker.init(frame, (x, y, w, h))
-while True:
+def capture_object_for_tracking(video_path):
+    cap = cv2.VideoCapture(video_path)
     ret, frame = cap.read()
-    if not ret:
-        break
+    bbox = cv2.selectROI('Frame', frame, fromCenter=False, showCrosshair=True)
+    tracker = cv2.legacy.TrackerMedianFlow_create()
+    tracker.init(frame, bbox)
+
     while True:
-        if cv2.waitKey(1) & 0xFF == ord("s"):
-            while True:
-                ret, frame = cap.read()
-                if not ret:
-                    break
-            tracker = cv2.legacy.TrackerMedianFlow_create()
-            ret, track_window = tracker.update(frame)
-            x, y, w, h = int(track_window[0]), int(track_window[1]), int(track_window[2]), int(track_window[3])
-            img2 = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.imshow('frame', img2)
-    while True:
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        ret, frame = cap.read()
+        if not ret:
             break
 
-cap.release()
-cv2.destroyAllWindows()
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord('r'):
+            bbox = cv2.selectROI('Frame', frame, fromCenter=False, showCrosshair=True)
+            tracker.init(frame, bbox)
+
+        success, bbox = tracker.update(frame)
+
+        if success:
+            # Рисуем прямоугольник вокруг объекта
+            (x, y, w, h) = [int(v) for v in bbox]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        cv2.imshow('Frame', frame)
+
+        # Остановка видео при нажатии клавиши 'q'
+        if key == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+capture_object_for_tracking('dvor1.mp4')
