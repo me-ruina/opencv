17d849dad069d72a58517c531e4acf2a787f1801
ruina.me <umewethey@gmail.com>
2024-02-12 15:58:55 +0400

    Ready

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+
 # Захват видеопотока с веб-камеры
 cap = cv2.VideoCapture(0)
 
@@ -15,23 +16,26 @@ while True:
         bbox = cv2.selectROI('Frame', frame, fromCenter=False, showCrosshair=True)
         tracker = cv2.legacy.TrackerMedianFlow_create()
         tracker.init(frame, bbox)
+
         while True:
             ret, frame = cap.read()
+
             if ret:
                 success, bbox = tracker.update(frame)
 
-            if success:
-                # Рисуем прямоугольник вокруг объекта
-                (x, y, w, h) = [int(v) for v in bbox]
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-            cv2.imshow('Frame', frame)
+                if success:
+                    # Рисуем прямоугольник вокруг объекта
+                    (x, y, w, h) = [int(v) for v in bbox]
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                cv2.imshow('Frame', frame)
 
             # Остановка видео при нажатии клавиши 'q'
             key = cv2.waitKey(1) & 0xFF
-
             if key == ord('q'):
                 break
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # Освобождение ресурсов и закрытие окон
 cap.release()
