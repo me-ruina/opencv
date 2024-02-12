193aef5aabb3534fcf84176a8ff483f15f8ae1e4
ruina.me <umewethey@gmail.com>
2024-02-12 15:54:06 +0400

    более менее

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
 import cv2
+# Захват видеопотока с веб-камеры
+cap = cv2.VideoCapture(0)
 
-cap = cv2.VideoCapture('dvor1.mp4')
-ret, frame = cap.read()
-bbox = cv2.selectROI('Frame', frame, fromCenter=False, showCrosshair=True)
-tracker = cv2.legacy.TrackerMedianFlow_create()
-tracker.init(frame, bbox)
+while True:
+    # Считывание кадра с веб-камеры
+    ret, frame = cap.read()
+
+    if ret:
+        # Отображение кадра в окне
+        cv2.imshow('Webcam', frame)
+
+    # Для завершения приложения нажмите 'q'
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        bbox = cv2.selectROI('Frame', frame, fromCenter=False, showCrosshair=True)
+        tracker = cv2.legacy.TrackerMedianFlow_create()
+        tracker.init(frame, bbox)
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                success, bbox = tracker.update(frame)
+
+            if success:
+                # Рисуем прямоугольник вокруг объекта
+                (x, y, w, h) = [int(v) for v in bbox]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+            cv2.imshow('Frame', frame)
+
+            # Остановка видео при нажатии клавиши 'q'
+            key = cv2.waitKey(1) & 0xFF
+
+            if key == ord('q'):
+                break
+
+# Освобождение ресурсов и закрытие окон
 cap.release()
-tracker.update(frame)
-def capture_object_for_tracking(video_path):
-    global cap, ret, frame, bbox, tracker
-
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        key = cv2.waitKey(1) & 0xFF
-
-        if key == ord('r'):
-            bbox = cv2.selectROI('Frame', frame, fromCenter=False, showCrosshair=True)
-            tracker.init(frame, bbox)
-
-        success, bbox = tracker.update(frame)
-
-        if success:
-            # Рисуем прямоугольник вокруг объекта
-            (x, y, w, h) = [int(v) for v in bbox]
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-        cv2.imshow('Frame', frame)
-
-        # Остановка видео при нажатии клавиши 'q'
-        if key == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
+cv2.destroyAllWindows()
